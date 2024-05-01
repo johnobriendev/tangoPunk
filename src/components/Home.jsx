9c2b1d2f7d994d53a6f7a2262e7f06c69a8f533c
John O'Brien <johnobrien.dev@gmail.com>
2024-05-01 19:33:36 -0400

    fix z index on home page nav

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
 export default function Home(){
     return(
-        <div className="w-full h-full md:text-white">
+        <div className="w-full h-full z-0 md:text-white">
             <img src="demian_dark.jpeg" alt="" className=" mb-8 hidden md:block md:w-full md:h-screen md:z-0 "/>
             <img src="demian_boat.jpeg" alt="" className="h-auto w-full  md:hidden" />
-            <h2 className="text-xl absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:top-100 md:left-64">Músico, Artista, Compositor</h2>
+            <h2 className="text-xl absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-5 md:top-100 md:left-64">Músico, Artista, Compositor</h2>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ export default function Navbar(){
    const [menuOpen, setMenuOpen] = useState(false);
    
     return(
-        <nav className="flex flex-col items-start gap-5  py-5 px-2 sticky bg-yellow-500 text-red-500 w-full top-0 md:flex-row md:justify-between md:items-center md:px-10 md:bg-transparent md:text-white md:z-10 md:absolute">
+        <nav className="flex flex-col items-start gap-5  py-5 px-2 sticky bg-yellow-500 text-red-500 w-full top-0  z-10 md:flex-row md:justify-between md:items-center md:px-10 md:bg-transparent md:text-white md:z-10 md:absolute">
             <Link to='/' className="text-3xl md:hover:scale-110 duration-150 px-3 py-1 rounded md:text-5xl" onClick={() => setMenuOpen(false)}>tangopunk</Link>
             
                 <div className="text-2xl md:hidden cursor-pointer absolute top-5 right-5" onClick={() => setMenuOpen(!menuOpen)}>
                     <RxHamburgerMenu />
                 </div>
-                <ul className={menuOpen ? "flex flex-col gap-5 w-screen h-screen " : "hidden md:flex md:text-xl"}>
+                <ul className={menuOpen ? "flex flex-col gap-5 w-screen h-screen z-10 " : "hidden md:flex md:text-xl"}>
                     <li><NavLink to='/about' className=' hover:bg-slate-700 p-2 rounded' onClick={() => setMenuOpen(false)}>About</NavLink></li>
                     <li><NavLink to='/music' className=' hover:bg-slate-700 p-2 rounded' onClick={() => setMenuOpen(false)}>Music</NavLink></li>
                     <li><NavLink to='contact' className=' hover:bg-slate-700 p-2 rounded' onClick={() => setMenuOpen(false)}>Contact</NavLink></li>
