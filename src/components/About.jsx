f6147b68ee32f0d01665960ef9bc5b69b652aab6
John O'Brien <johnobrien.dev@gmail.com>
2024-05-07 19:24:34 -0400

    update about me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About(){
     return(
         <div className="p-6 leading-loose overflow-y-scroll pb-[150px]">
             <h2 className="text-2xl text-center mb-2">About</h2>
-            <img src="johnguitar.jpg" alt="" className="mb-5"/>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit ullam sed nulla excepturi aut, consequatur, numquam similique nihil rerum harum reprehenderit earum, ab nobis officia corporis quis velit labore error commodi fugit ea amet molestiae! Excepturi molestias a ipsam quod porro? Corrupti soluta recusandae, non nulla explicabo ad! Earum vero quos, odit blanditiis rerum animi labore atque corrupti ipsum enim minima fuga quaerat tenetur officiis, deserunt consectetur pariatur inventore tempore sapiente quae eveniet. Numquam culpa vitae, quasi totam maxime modi a facilis illum consectetur itaque ipsam dolore ullam. Dignissimos sed nesciunt harum iusto aspernatur nobis, dolor totam sint tempore explicabo.</p>
             <div></div>
         </div>
     )
