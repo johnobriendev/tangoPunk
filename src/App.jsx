19efc0ab3b5c1aae2d07f743f997bbc5e59d8a91
John O'Brien <johnobrien.dev@gmail.com>
2024-05-18 22:30:25 -0400

    remove overflow scroll on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom'
 function App() {
 
   return (
-    <div className='relative flex flex-col w-full h-screen overflow-auto'>
+    <div className='relative flex flex-col w-full h-screen overflow-auto p-0 m-0'>
       <Navbar />
       <Outlet className="flex-1"/>
       <Footer/>
